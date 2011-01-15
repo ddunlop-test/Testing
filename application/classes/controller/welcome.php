ad02cf1783b73a8b6e774169aed1a41b2bb6060d
ddunlop <github2@ddunlop.oterinbox.com>
2011-01-15 06:49:18 -0500

    Switched to using kohana

---
+++ application/classes/controller/welcome.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Controller_Welcome extends Controller {
+  public function action_index() {
+    echo 'welcome';
+
+    Kohana_Log::instance()->add(Kohana::ERROR, 'test');
+  }
+}
